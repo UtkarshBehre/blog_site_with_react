5a88d0d66c9b0919e7a673665a58cd6ed394c951
Utkarsh Behre <utkarsh@Utkarshs-MacBook-Pro.local>
2020-08-16 19:20:31 -0700

    Demo commit

---
+++ src/build/precache-manifest.2e74213cf13ebcbdecd85f8415efd39c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "021b6cb9cf96e587aaf52e6fc14c168b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1491feb7f8016b63385b",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "bf868f1b43a2fb1c90a5",
+    "url": "/static/js/2.120d60d7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.120d60d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1491feb7f8016b63385b",
+    "url": "/static/js/main.1999e1eb.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
---
+++ src/server.js
@@ -0,0 +1,84 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { MongoClient } from 'mongodb';
+import path from 'path';
+/* to start server 
+normal start
+npx babel-node src/server.js
+
+/*
+
+/*
+start using nodemon for auto change auto restart
+npm install --save-dev nodemon
+npx nodemon --exec npx babel-node src/server.js
+
+mongodb
+mongod --dbpath=/Users/user/data/db
+db
+int new tab enter mongo for mongo shell
+
+*/
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '/build')));
+app.use(bodyParser.json());
+
+const withDB = async (operations, res) => {
+    try {
+        const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
+        const db = client.db('my-app');
+    
+        await operations(db);
+    
+        client.close();
+    } catch (error){
+        res.status(500).json({message: 'Error connecting to db', error});
+    }
+}
+app.get('/api/articles/:name', async (req, res) => {
+    withDB(async (db) => {
+        const articleName = req.params.name;
+ 
+        const articleInfo = await db.collection('articles').findOne({name: articleName });
+        res.status(200).json(articleInfo);
+    }, res);
+});
+
+app.post('/api/articles/:name/upvote', async (req, res) => {
+    const articleName = req.params.name; 
+    withDB(async (db) => {
+        const articleInfo = await db.collection('articles').findOne({name: articleName });
+        await db.collection('articles').updateOne({name: articleName},{
+             '$set': {
+                 upvotes: articleInfo.upvotes + 1,
+             },
+         });
+         const updatedArticleInfo = await db.collection('articles').findOne({name: articleName });
+         res.status(200).json(updatedArticleInfo);
+    }, res);
+});
+
+app.post('/api/articles/:name/add-comment', (req, res) => {
+    
+    const { username, text } = req.body;
+    const articleName = req.params.name;
+    withDB(async (db) =>{
+        const articleInfo = await db.collection('articles').findOne({name: articleName });
+        await db.collection('articles').updateOne({name: articleName},{
+            '$set': {
+                comments: articleInfo.comments.concat({username, text}),
+            },
+        });
+        const updatedArticleInfo = await db.collection('articles').findOne({name: articleName });
+        res.status(200).json(updatedArticleInfo);
+
+    }, res);
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/build/index.html'));
+})
+
+app.listen(8000, () => console.log("Listening on port 8000"));
